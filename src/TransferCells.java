7be46cfcb64c4725d5438c2f1dd9c6fcb5db1214
Vincent Simpson <34716206+vincentsimpson@users.noreply.github.com>
2018-09-13 23:01:27 -0400

    first commit

---
+++ src/TransferCells.java
@@ -0,0 +1,271 @@
+/**
+ * TODO: Implement a method that inserts a new "Transfer from Trust to General" table.
+ * 
+ */
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.util.RegionUtil;
+import org.apache.poi.xssf.usermodel.examples.CreateCell;
+
+public class TransferCells {
+
+	private static ArrayList<Integer> rowNumsOfValidTransferCells = new ArrayList<>();
+
+	public static void main(String[] args) throws InvalidFormatException, IOException {
+
+		File analysisSpreadsheet = new File("analysisSpreadsheet.xlsx");
+		File exportedSpreadsheet = new File("exportedexcelquickbooks.xlsx");
+
+		FileInputStream inputAnalysis = new FileInputStream(analysisSpreadsheet);
+		FileInputStream inputExported = new FileInputStream(exportedSpreadsheet);
+		Workbook analysisWB = WorkbookFactory.create(inputAnalysis);
+		Workbook exportedWB = WorkbookFactory.create(inputExported);
+		Sheet analysisSheet = analysisWB.getSheetAt(0);
+		Sheet exportedSheet = exportedWB.getSheetAt(0);
+
+		int rowStartAnalysis = 7;
+		int rowStartExported = 7;
+		int rowEndAnalysis = 7;
+		int rowEndExported = 0;
+
+		for (int rowNum = rowStartAnalysis; rowNum <= analysisSheet.getLastRowNum(); rowNum++) {
+			Row r = analysisSheet.getRow(rowNum);
+			if(isRowEmpty(r)) {
+				break;
+			} else {
+				rowEndAnalysis++;
+			}
+		}
+
+		for(int rowNum=rowStartExported; rowNum < exportedSheet.getLastRowNum(); rowNum++) {
+			try {
+				Row r = exportedSheet.getRow(rowNum);
+				Cell c = r.getCell(9);
+
+				if(c.toString().isEmpty()) {break;} 
+				else {rowEndExported++;}
+
+			} catch (NullPointerException e) {
+				break;
+			}
+		} rowEndExported += 7;
+		
+		int exportedNumOfRows = numOfRowsToInsertFromExport(analysisSheet, exportedSheet, rowEndAnalysis, rowEndExported);
+
+		analysisSheet.shiftRows(rowEndAnalysis, analysisSheet.getLastRowNum(), exportedNumOfRows);
+		for(int i=0; i < exportedNumOfRows + 2; i++) {
+			analysisSheet.createRow(329 + i);
+		}
+
+		transferCells(analysisSheet, exportedSheet, rowNumsOfValidTransferCells, rowEndAnalysis);
+		
+		createNewTransferForm(analysisWB, analysisSheet);
+
+		inputAnalysis.close();
+		inputExported.close();
+
+		analysisWB.write(new FileOutputStream(analysisSpreadsheet));
+		exportedWB.write(new FileOutputStream(exportedSpreadsheet));
+		exportedWB.close();
+		analysisWB.close();
+		
+		System.out.println(rowEndExported + " :: exported rows");
+		System.out.println(rowEndAnalysis + " :: analysis rows");
+		System.out.println(exportedNumOfRows + " :: added rows");
+	}
+	
+	/**
+	 * 
+	 * @param analysisSheet
+	 * @param exportedSheet
+	 * @param rowEndAnalysis
+	 * @param rowEndExported
+	 * @return
+	 */
+	public static int getNumberOfRowsToTransfer(Sheet analysisSheet, Sheet exportedSheet, int rowEndAnalysis, int rowEndExported) {
+		int numOfRowsToTransfer = numOfRowsToInsertFromExport(analysisSheet, exportedSheet, rowEndAnalysis, rowEndExported);
+		return numOfRowsToTransfer;
+	}
+
+	/**
+	 * 
+	 * @param analysis
+	 * @param exported
+	 * @param rowsToTransfer
+	 * @param rowEndAnalysis
+	 */
+	public static void transferCells(Sheet analysis, Sheet exported, ArrayList<Integer> rowsToTransfer, int rowEndAnalysis) {
+		int exportDateCol = 1;
+		int exportTransactionTypeCol = 2;
+		int exportCheckNumCol = 3;
+		int exportNameCol = 4;
+		int exportMemoCol = 5;
+		int exportAmountCol = 8;
+		
+		ArrayList<String> dateCellsFromExport = new ArrayList<String>();
+		ArrayList<String> transTypeCellsFromExport = new ArrayList<String>();
+		ArrayList<Integer> checkNumCellsFromExport = new ArrayList<Integer>();
+		ArrayList<String> nameCellsFromExport = new ArrayList<String>();
+		ArrayList<String> memoCellsFromExport = new ArrayList<String>();
+		ArrayList<Double> amountCellsFromExport = new ArrayList<Double>();
+
+
+		for (int i=0; i < rowsToTransfer.size(); i++ ) {
+			dateCellsFromExport.add(exported.getRow(rowsToTransfer.get(i)).getCell(exportDateCol).toString());			
+			transTypeCellsFromExport.add(exported.getRow(rowsToTransfer.get(i)).getCell(exportTransactionTypeCol).toString());					
+			checkNumCellsFromExport.add((int) exported.getRow(rowsToTransfer.get(i)).getCell(exportCheckNumCol).getNumericCellValue());
+			nameCellsFromExport.add(exported.getRow(rowsToTransfer.get(i)).getCell(exportNameCol).toString());
+			memoCellsFromExport.add(exported.getRow(rowsToTransfer.get(i)).getCell(exportMemoCol).toString());
+			
+			Double z = exported.getRow(rowsToTransfer.get(i)).getCell(exportAmountCol).getNumericCellValue();
+			String x = String.format("%.2f", z);
+			Double dubtwo = Double.parseDouble(x);
+
+			amountCellsFromExport.add(dubtwo);
+
+			Cell a = analysis.createRow(rowEndAnalysis).createCell(exportDateCol-1);
+			a.setCellType(CellType.STRING);
+			a.setCellValue(dateCellsFromExport.get(i));
+
+			Cell b = analysis.getRow(rowEndAnalysis).createCell(exportTransactionTypeCol-1);
+			b.setCellType(CellType.STRING);
+			b.setCellValue(transTypeCellsFromExport.get(i));
+
+			Cell c = analysis.getRow(rowEndAnalysis).createCell(exportCheckNumCol-1);
+			c.setCellType(CellType.STRING);
+			if(checkNumCellsFromExport.get(i) != 0) {
+				c.setCellValue(checkNumCellsFromExport.get(i));
+			}
+
+			Cell d = analysis.getRow(rowEndAnalysis).createCell(exportNameCol-1);
+			d.setCellType(CellType.STRING);
+			d.setCellValue(nameCellsFromExport.get(i));
+
+			Cell e = analysis.getRow(rowEndAnalysis).createCell(exportMemoCol-1);
+			e.setCellType(CellType.STRING);
+			e.setCellValue(memoCellsFromExport.get(i));
+
+			Cell f = analysis.getRow(rowEndAnalysis).createCell(exportAmountCol-1);
+			f.setCellType(CellType.NUMERIC);
+			f.setCellValue(amountCellsFromExport.get(i));
+
+			rowEndAnalysis++;
+
+		}
+	}
+
+	/**
+	 * 
+	 * @param row
+	 * @return
+	 */
+	public static boolean isRowEmpty(Row row) {
+		if (row == null) {
+			return true;
+		}
+		if (row.getLastCellNum() <= 0) {
+			return true;
+		}
+		for(int cellNum = row.getFirstCellNum(); cellNum < row.getLastCellNum(); cellNum++) {
+			Cell cell = row.getCell(cellNum);
+			if(cell != null && cell.getCellTypeEnum() != CellType.BLANK && !cell.toString().isEmpty()) {
+				return false;
+			}
+		}return true;
+	}
+
+	/**
+	 * 
+	 * @param analysis
+	 * @param export
+	 * @param rowEndAnalysis
+	 * @param rowEndExported
+	 * @return
+	 */
+	public static int numOfRowsToInsertFromExport(Sheet analysis, Sheet export, int rowEndAnalysis, int rowEndExported) {
+		//Need to figure out how many rows to insert into the analysis from the exported sheet
+		int rowCount =0;
+		for(int rowNumExport=7; rowNumExport < rowEndExported; rowNumExport++) {
+			for(int rowNumAnalysis= rowEndAnalysis-1; rowNumAnalysis > 300; rowNumAnalysis--) {
+				try {
+					Cell currentExportCell = export.getRow(rowNumExport).getCell(9);
+					Cell currentAnalysisCell = analysis.getRow(rowNumAnalysis).getCell(8);
+
+					String exportedValue = String.format("%.2f", currentExportCell.getNumericCellValue()); 
+					String analysisValue = String.format("%.2f", currentAnalysisCell.getNumericCellValue());
+
+					if(!(exportedValue.equals(analysisValue))) {
+						rowNumsOfValidTransferCells.add(rowNumExport);
+						rowCount++;
+						break;
+					} else {
+						break;
+					}
+				} catch (Exception exception) {
+					System.out.println(rowNumExport + " ::: " + rowNumAnalysis);
+					exception.printStackTrace();
+				}
+			}
+		}return rowCount;
+	}
+	
+	/**
+	 * 	
+	 * @param wb
+	 * @param analysis
+	 */
+	public static void createNewTransferForm(Workbook wb, Sheet analysis) {
+		
+		int analysisLastRowNum = analysis.getLastRowNum() + 2;
+		
+		Row row = analysis.createRow(analysisLastRowNum);
+		Cell cell1 = row.createCell(6);
+		
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+		String dateString = sdf.format(date);
+		
+		cell1.setCellValue("To Be Transferred " + dateString);
+		
+		CellStyle cellStyle = wb.createCellStyle();
+		cellStyle.setAlignment(HorizontalAlignment.CENTER);
+		cellStyle.setVerticalAlignment(VerticalAlignment.BOTTOM);
+		
+		CellRangeAddress rangeAddress = new CellRangeAddress(analysisLastRowNum, analysisLastRowNum, 6, 8 );
+		
+		Font font = wb.createFont();
+		font.setFontName("Arial");
+		font.setFontHeightInPoints((short)11);
+		font.setBold(true);
+		cellStyle.setFont(font);
+		cell1.setCellStyle(cellStyle);
+		analysis.addMergedRegion(rangeAddress);
+		RegionUtil.setBorderBottom(BorderStyle.THIN, rangeAddress, analysis);		
+
+		
+		
+	}
+	
+}
